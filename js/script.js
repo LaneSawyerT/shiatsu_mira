0f20bdb7000abf0f01cb19ac4346e1f4550c1dfc
Matt Rudge <mrudge@gmail.com>
2019-12-10 20:46:08 +0000

    Added back switching languages

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     fileName = location.href.split("/").slice(-1);
-//     if (localStorage.shiatsuMira) {
-//         if (localStorage.shiatsuMira == "IT" && fileName == "index.html") {
-//             window.location = "./index_it.html";
-//         }
-//     }
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    fileName = location.href.split("/").slice(-1);
+    if (localStorage.shiatsuMira) {
+        if (localStorage.shiatsuMira == "IT" && fileName == "index.html") {
+            window.location = "./index_it.html";
+        }
+    }
+});
 
-// function selectEnglish() {
-//     localStorage.shiatsuMira = "EN";
-//     window.location = "./index.html";
-// }
+function selectEnglish() {
+    localStorage.shiatsuMira = "EN";
+    window.location = "./index.html";
+}
 
-// function selectItalian() {
-//     localStorage.shiatsuMira = "IT";
-//     window.location = "./index_it.html";
-// }
+function selectItalian() {
+    localStorage.shiatsuMira = "IT";
+    window.location = "./index_it.html";
+}
